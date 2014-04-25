44500ef2254a01d5c07c95879683f688c2e98c95
Robin <robindoble@gmai.com>
2014-04-25 15:49:05 +0100

    added a Takeaway Class

---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,20 @@
+require 'takeaway'
+
+
+describe Takeaway do 
+
+	context 'creating a menu' do
+
+		it 'should present a menu of options' do
+			expect(Takeaway.new.class).to be_true
+		end
+
+	
+
+
+
+	end
+
+
+
+end
